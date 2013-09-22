283ab680eeadb45e3b2b61a892fbde23ad38b183
Tomek Mrugalski <thomson@klub.com.pl>
2013-09-22 22:52:57 +0200

    Sections are now converted properly!

--- nroff2xml.py
+++ nroff2xml.py
@@ -3,6 +3,8 @@
 # Author: Tomek Mrugalski
 #
 
+VERSION = '0.0.1'
+
 import sys
 import re
 
@@ -10,151 +12,244 @@ import re
 
 PREAMBLE = '<?xml version="1.0" encoding="US-ASCII"?>\n';
 DOCTYPE_BEGIN = '<!DOCTYPE rfc SYSTEM "rfc2629.dtd" [\n'
-
 DOCTYPE_END = ']>\n'
-
 STYLESHEET = '<?xml-stylesheet type="text/xsl" href="rfc2629.xslt" ?>'
-
 HEADER_STRICT='<?rfc strict="yes" ?>\n'
 HEADER_TOC='<?rfc toc="yes"?>\n'
+HEADER_TOC_DEPTH='<?rfc tocdepth="4"?>\n'
+HEADER_SYMREFS='<?rfc symrefs="yes"?>\n'
+HEADER_SORTREFS='<?rfc sortrefs="yes" ?>\n'
+HEADER_COMPACT='<?rfc compact="yes" ?>\n'
+HEADER_SUBCOMPACT='<?rfc subcompact="no" ?>\n'
+HEADER_CATEGORY='<rfc category="info" docName="draft-ietf-xml2rfc-template-05" ipr="trust200902">\n'
+
+AUTHOR_TEMPLATE="""<author fullname="Elwyn Davies" initials="E.B." role="editor"
+            surname="Davies">
+      <organization>Folly Consulting</organization>
+      <address>
+        <postal>
+          <street></street>
+          <!-- Reorder these if your country does things differently -->
+          <city>Soham</city>
+          <region></region>
+          <code></code>
+          <country>UK</country>
+        </postal>
+        <phone>+44 7889 488 335</phone>
+        <email>elwynd@dial.pipex.com</email>
+        <!-- uri and facsimile elements may also be added -->
+      </address>
+    </author>"""
 
 
+class Nroff2Xml:
+    xml=''
+    nroff=''
+
+    def readNroff(self, infile):
+        with open(infile) as f:
+            self.nroff = f.readlines()
+
+        print("Read %d lines from %s file." % (len(self.nroff), infile))
+
+    def startSection(self, sections_list, lineno, section, section_title):
+        sections_list.append([section, section_title, lineno])
+        print("Starting section %s (%s) in line %d." % (section, section_title, lineno))
+
+        self.xml += '<section title="' + section_title + '"> <!-- ' + section + ', line ' + str(lineno) + '-->\n'
+
+        return sections_list
+
+    def endSection(self, sections_list):
+        end_section = sections_list.pop()
+        print("Ending section " + end_section[0] + ", started in line " + str(end_section[2]))
+
+        self.xml += '</section> <!-- ends: "' + end_section[0] + " from line " + str(end_section[2]) + '-->\n'
+
+        return sections_list
+
+    def addPreamble(self):
+        self.xml += PREAMBLE
+        self.xml += DOCTYPE_BEGIN
+        self.xml += DOCTYPE_END
+        self.xml += STYLESHEET
+        self.xml += HEADER_STRICT
+        self.xml += HEADER_TOC
+        self.xml += HEADER_TOC_DEPTH
+        self.xml += HEADER_SYMREFS
+        self.xml += HEADER_SORTREFS
+        self.xml += HEADER_COMPACT
+        self.xml += HEADER_SUBCOMPACT
+        self.xml += HEADER_CATEGORY
+
+    def extractTitle(self):
+        return "Unknown"
+
+    def extractShortTitle(self):
+        return "Unknown(short)"
+
+    def extractDate(self):
+        return ["1", "January", "1900"]
+
+    def addFront(self):
+        self.xml += "<front>\n"
+        self.xml += '<title abbrev="' + self.extractShortTitle() +'">' + self.extractTitle() + '</title>\n'
+
+        self.xml += AUTHOR_TEMPLATE
+
+        date = self.extractDate()
+        self.xml += '<date day="' + date[0] + '" month="' + date[1] + '" year="' + date[2] + '" />\n'
+
+        self.xml += "</front>\n\n"
 
 
+    def convert(self):
 
-def readNroff(infile):
-    with open(infile) as f:
-        content = f.readlines()
+        section_re = re.compile("^\s*(\d+\.)(\d+\.)?(\d+\.)?(\d+\.)? (.+)*$") # Matches section number
+        dotti0_re = re.compile("^\.ti\s*0\s*$") # matches .ti 0
 
-    print("Read %d lines from %s file." % (len(content), infile))
-
-    return content
-
-def convert(nroff):
-    xml = PREAMBLE
-    xml += DOCTYPE_BEGIN
-
-    section_re = re.compile("^\s*(\d+\.)(\d+\.)?(\d+\.)?(\d+\.)? (.+)*$") # Matches section number
-    dotti0_re = re.compile("^\.ti\s*0\s*$") # matches .ti 0
-
-    likely_section = False
-
-    # List of section info structures
-    sections_list = []
-
-    lineno = 0
-    for line in nroff:
-        lineno += 1
-
-        if dotti0_re.search(line):
-            likely_section = True
-            #print ("### Found section begin in line %d" % lineno)
-            continue
-
-        s = section_re.search(line)
-
-        section_lv1 = 0
-        section_lv2 = 0
-        section_lv3 = 0
-        section_lv4 = 0
-
-        if (s is not None):
-            #print (s.groups(), s.groups()[0])
-
-            # get 0th group, remove the last char (.) and convert to int
-            section_lv1 = int(s.groups()[0][:-1])
-
-            if s.groups()[1] is None:
-                level = 1
-            else:
-                if s.groups()[2] is None:
-                    level = 2
-                    section_lv2 = int(s.groups()[1][:-1])
-                else:
-                    if s.groups()[3] is None:
-                        level = 3
-                        section_lv2 = int(s.groups()[1][:-1])
-                        section_lv3 = int(s.groups()[2][:-1])
-                    else:
-                        level = 4
-                        section_lv2 = int(s.groups()[1][:-1])
-                        section_lv3 = int(s.groups()[2][:-1])
-                        section_lv4 = int(s.groups()[3][:-1])
-
-            if level > len(sections_list) + 1:
-                print("\nError in line %d: parser thinks that the current section nest level is %d,\n"\
-                      "but encountered a line that looks like level %d (%s). Sections levels can\n"\
-                      "only increase by one.\n" % (lineno, len(sections_list), level, line))
-                sys.exit(1)
-            
-            section_title = s.groups()[4]
-
-            if level == 1:
-                section = ("%d" % section_lv1)
-            if level == 2:
-                section = ("%d.%d" % (section_lv1, section_lv2))
-            if level == 3:
-                section = ("%d.%d.%d" % (section_lv1, section_lv2, section_lv3))
-            if level == 4:
-                section = ("%d.%d.%d.%d" % (section_lv1, section_lv2, section_lv3, section_lv4))
-
-            if level > len(sections_list):
-                sections_list = startSection(sections_list, lineno, section, section_title)
-            else:
-                if level == len(sections_list):
-                    sections_list = endSection(sections_list)
-                    sections_list = startSection(sections_list, lineno, section, section_title)
-                else:
-                    while level <= len(sections_list):
-                        print("New level: %d, existing level: %d" % (level, len(sections_list)))
-                        sections_list = endSection(sections_list)
-                    sections_list = startSection(sections_list, lineno, section, section_title)
+        self.xml += "<middle>\n"
 
         likely_section = False
 
-    return xml
+        # List of section info structures
+        sections_list = []
 
-def startSection(sections_list, lineno, section, section_title):
-    sections_list.append([lineno, section])
-    print("Starting section %s (%s) in line %d." % (section, section_title, lineno))
+        lineno = 0
+        for line in self.nroff:
+            lineno += 1
 
-    return sections_list
+            if dotti0_re.search(line):
+                likely_section = True
+                #print ("### Found section begin in line %d" % lineno)
+                continue
 
-def endSection(sections_list):
-    end_section = sections_list.pop()
-    print(">")
-    return sections_list
+            s = section_re.search(line)
 
-def writeXml(xml, outfile):
-    f = open(outfile, "w")
+            section_lv1 = 0
+            section_lv2 = 0
+            section_lv3 = 0
+            section_lv4 = 0
 
-    f.write(xml)
+            if (s is not None):
+                #print (s.groups(), s.groups()[0])
 
-    f.close()
+                # get 0th group, remove the last char (.) and convert to int
+                section_lv1 = int(s.groups()[0][:-1])
 
-def main(argv):
-    if (len(argv)<1):
-        print("At least one parameter is required: nroff input file")
-        exit(-1)
+                if s.groups()[1] is None:
+                    level = 1
+                else:
+                    if s.groups()[2] is None:
+                        level = 2
+                        section_lv2 = int(s.groups()[1][:-1])
+                    else:
+                        if s.groups()[3] is None:
+                            level = 3
+                            section_lv2 = int(s.groups()[1][:-1])
+                            section_lv3 = int(s.groups()[2][:-1])
+                        else:
+                            level = 4
+                            section_lv2 = int(s.groups()[1][:-1])
+                            section_lv3 = int(s.groups()[2][:-1])
+                            section_lv4 = int(s.groups()[3][:-1])
 
-    infile = argv[0]
-    outfile = argv[0]
-    if (outfile.endswith(".nroff")):
-        outfile = outfile[:-6] + ".xml"
+                if level > len(sections_list) + 1:
+                    print("\nError in line %d: parser thinks that the current section nest level is %d,\n"\
+                          "but encountered a line that looks like level %d (%s). Sections levels can\n"\
+                          "only increase by one.\n" % (lineno, len(sections_list), level, line))
+                    sys.exit(1)
 
-    if (len(argv)>=2):
-        outfile = argv[1]
+                section_title = s.groups()[4]
 
-    print("Reading %s" % infile)
-    nroff = readNroff(infile)
-    print("Read %d lines." % len(nroff))
+                if level == 1:
+                    section = ("%d" % section_lv1)
+                if level == 2:
+                    section = ("%d.%d" % (section_lv1, section_lv2))
+                if level == 3:
+                    section = ("%d.%d.%d" % (section_lv1, section_lv2, section_lv3))
+                if level == 4:
+                    section = ("%d.%d.%d.%d" % (section_lv1, section_lv2, section_lv3, section_lv4))
 
-    print("Converting %s => %s" % (infile, outfile))
-    xml = convert(nroff)
+                if level > len(sections_list):
+                    sections_list = self.startSection(sections_list, lineno, section, section_title)
+                else:
+                    if level == len(sections_list):
+                        sections_list = self.endSection(sections_list)
+                        sections_list = self.startSection(sections_list, lineno, section, section_title)
+                    else:
+                        while level <= len(sections_list):
+                            sections_list = self.endSection(sections_list)
+                        sections_list = self.startSection(sections_list, lineno, section, section_title)
 
-    print("Writing XML output (%d bytes) to %s" % (len(xml), outfile))
+            likely_section = False
 
-    writeXml(xml, outfile)
+        while len(sections_list):
+            sections_list = self.endSection(sections_list)
+
+        self.xml += "</middle>\n"
+
+        return self.xml
+
+    def addReferences(self):
+        #self.xml += '<references title="Normative References">\n'
+        #self.xml += '</references>\n'
+        #self.xml += '<references title="Informative References">\n'
+        #self.xml += '</references>\n'
+        pass
+
+    def addBack(self):
+        self.xml += "<back>\n"
+
+        self.addReferences()
+
+        self.xml += "</back>\n"
+
+    def addPostamble(self, infile):
+        self.xml += "</rfc>\n"
+        self.xml += "<!-- generated from file " + infile + "with nroff2xml " + VERSION + " by Tomek Mrugalski -->\n"
+
+    def writeXml(self, outfile):
+
+        print("Writing XML output (%d bytes) to %s" % (len(self.xml), outfile))
+
+        f = open(outfile, "w")
+
+        f.write(self.xml)
+
+        f.close()
+
+    def main(argv):
+        if (len(argv)<1):
+            print("At least one parameter is required: nroff input file")
+            exit(-1)
+
+        infile = argv[0]
+        outfile = argv[0]
+        if (outfile.endswith(".nroff")):
+            outfile = outfile[:-6] + ".xml"
+
+        if (len(argv)>=2):
+            outfile = argv[1]
+
+        convert = Nroff2Xml()
+
+        convert.readNroff(infile)
+
+        convert.addPreamble()
+
+        # Authors, Abstract, keywords, meta-data, date, title
+        convert.addFront()
+
+        # The actual content (<middle>)
+        convert.convert()
+
+        # References and appendices (<back>)
+        convert.addBack()
+
+        convert.addPostamble(infile)
+
+        convert.writeXml(outfile)
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    Nroff2Xml.main(sys.argv[1:])
